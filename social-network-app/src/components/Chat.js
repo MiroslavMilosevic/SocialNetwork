12d5edbd8244d43f18cfe744dd72022ae0ea3bd2
unknown <miroslav.milosevic999@gmail.com>
2020-10-23 15:59:15 +0200

    comit 6 logika sa chatom je pri kraju

--- social-network-app/src/components/Chat.js
+++ social-network-app/src/components/Chat.js
@@ -1,33 +1,51 @@
-import React, { useEffect, useState, } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { BrowserRouter as Redirect, useParams } from 'react-router-dom'
-import { getUserMessages, } from '../services/axios'
+import { getUserMessages, postMessage, } from '../services/axios'
 import { v1 as uuid } from 'uuid'
 
 export default function Chat() {
-    const [id] = useState(useParams().id)
+    const [id] = useState(useParams().id);
     const [user] = useState({ id: localStorage.getItem('id'), username: localStorage.getItem('username'), password: localStorage.getItem('password') });
     const [nizPoruka, setNizPoruka] = useState([]);
-    const [porukePrijatelja, setPorukePrijatelja] = useState([])
-    const [zajednickePoruke, setZajednickePoruke] = useState([])
+    const [porukePrijatelja, setPorukePrijatelja] = useState([]);
+    const [zajednickePoruke, setZajednickePoruke] = useState([]);
+    const [inputPoruka, setInputPoruka] = useState('');
+    const intervalRef = useRef();
 
     useEffect(() => {
 
         getUserMessages(Number(id)).then(res => {
-            let tmp = res.data.map(el => { return { id: el.split(';')[0], poruka: el.split(';')[1], date: el.split(';')[2] } })
+            let tmp = res.data.map(el => { return { id: el.split(';')[0], poruka: el.split(';')[1], date:  Date.parse(el.split(';')[2]) } })
             setPorukePrijatelja(tmp);
         })
         getUserMessages(Number(user.id)).then(res => {
-            let tmp = res.data.map(el => { return { id: el.split(';')[0], poruka: el.split(';')[1], date: el.split(';')[2] } })
+            let tmp = res.data.map(el => { return { id: el.split('')[0], poruka: el.split(';')[1], date: Date.parse(el.split(';')[2]) } })
             console.log(tmp);
             setNizPoruka(tmp);
         })
+        setInterval(() => {
+            getUserMessages(Number(id)).then(res => {
+                let tmp = res.data.map(el => { return { id: el.split(';')[0], poruka: el.split(';')[1], date: Date.parse(el.split(';')[2]) } })
+                setPorukePrijatelja(tmp);
+                console.log('setInterval u useEffect-u');   
+            })
+        }, 1200);
+        setInterval(() => {
+            getUserMessages(Number(user.id)).then(res => {
+            let tmp = res.data.map(el => { return { id: el.split(';')[0], poruka: el.split(';')[1], date: Date.parse(el.split(';')[2]) } })
+            setNizPoruka(tmp);
+        })
+        console.log('updateeeeeeeeeeeeee');
+        }, 3200);
     }, [])
 
     useEffect(() => {
-        if (porukePrijatelja !== []) {
+     //   if (porukePrijatelja !== []) {       
             let tmp = [...porukePrijatelja, ...nizPoruka];
+            console.log(zajednickePoruke.sort((a,b)=> a.date-b.date));
             setZajednickePoruke(tmp);
-        }
+      //  }
+ console.log('useefecttttttttttt');
     }, [porukePrijatelja, nizPoruka])
 
 
@@ -37,8 +55,21 @@ export default function Chat() {
 
     return (
         <div>
-            {'cetujem sa korisnikom sa id-om: ' + id}
-            {zajednickePoruke.map(el => <p key={uuid()}>{el.poruka + ' | ' + el.id}</p>)}
+             {/* <button onClick={()=>{ setNizPoruka([])}}>DUGME</button> */}
+            <h1>{'chat sa korisnikom sa id-om: ' + id}</h1>
+            <input type='text' value={inputPoruka} placeholder='poruka...' onChange={(e)=>{setInputPoruka(e.target.value)} } onKeyPress={(event)=>{
+                    if (event.key == 'Enter') {
+                       postMessage(inputPoruka, user.id)
+                       let tmp=[...nizPoruka];
+                       tmp.push( { id: user.id, poruka:inputPoruka,   date:new Date().toString() } );
+                     //  let objekat={ id: user.id, poruka:inputPoruka,   date:new Date().toString() }
+                       setNizPoruka(tmp);
+                     }
+                  
+            }}/>
+            {zajednickePoruke.sort((a,b)=>a.date-b.date)
+            .map(el => <p key={uuid()}>{el.poruka + ' | ' + el.id+ '   |  datum: '+el.date}</p>)
+            }
         </div>
     )
 }
--- social-network-app/src/services/axios.js
+++ social-network-app/src/services/axios.js
@@ -4,7 +4,7 @@ export const getUserMessages=(id)=>{
    return axios.get(`https://script.google.com/macros/s/AKfycbyaPSgO1KOnrE1vPswfyrByQwhcMtJH5t8VfErd_pc3viSAkU4/exec?id=${id+1}`)
 }
 
-export const postMessage=(inputPoruka)=>{
+export const postMessage=(inputPoruka, idUsera)=>{
 
-    axios.post(`https://script.google.com/macros/s/AKfycbyaPSgO1KOnrE1vPswfyrByQwhcMtJH5t8VfErd_pc3viSAkU4/exec?poruka=${inputPoruka}&id=1`)
+    axios.post(`https://script.google.com/macros/s/AKfycbyaPSgO1KOnrE1vPswfyrByQwhcMtJH5t8VfErd_pc3viSAkU4/exec?poruka=${inputPoruka}&id=${idUsera}`)
  }
