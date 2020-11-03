9d87337593968d9574a50ac652da8d69204036e5
unknown <miroslav.milosevic999@gmail.com>
2020-11-03 20:20:37 +0100

    Updates

---
+++ precache-manifest.3e63803b7e2e7cded33672d72774e90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d069956eb1426efc6f2b70aae90a362",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "23b372691032348e3ed7",
+    "url": "/SocialNetwork/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b46b0f68fe9d282e2074",
+    "url": "/SocialNetwork/static/js/2.175c127c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SocialNetwork/static/js/2.175c127c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b372691032348e3ed7",
+    "url": "/SocialNetwork/static/js/main.e7619bab.chunk.js"
+  },
+  {
+    "revision": "75223e7ec467ff32eec9",
+    "url": "/SocialNetwork/static/js/runtime-main.239c1b25.js"
+  }
+]);
